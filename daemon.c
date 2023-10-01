8930d7a8e66720966fc09b7688ab9fbe99d536e3
Elmir <elmirz@MSI>
2023-10-01 13:34:11 +0200

    init daemon

---
+++ daemon.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdlib.h>		/* free */
+#include <stdio.h>		/* printf */
+#include <unistd.h>		/* fgets */
+#include <string.h>		/* memset */
+#include <sys/socket.h>	/* socket */
+#include <fcntl.h>
+#include <sys/epoll.h>			/* epoll */
+#include <linux/if_packet.h>	/* AF_PACKET */
+#include <net/ethernet.h>		/* ETH_* */
+#include <arpa/inet.h>			/* htons */
+#include <ifaddrs.h>			/* getifaddrs */
+
+
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
